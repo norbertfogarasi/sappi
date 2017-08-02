4bb97d2f26d7f1e1bee172e66bfdd727520a5583
norbertfogarasi <norbert.fogarasi.it@gmail.com>
2017-08-02 16:33:55 +0300

    Car detail upgraded

--- app/src/main/java/com/lynxsolutions/intern/sappi/cars/RouteDetailFragment.java
+++ app/src/main/java/com/lynxsolutions/intern/sappi/cars/RouteDetailFragment.java
@@ -3,9 +3,12 @@ package com.lynxsolutions.intern.sappi.cars;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lynxsolutions.intern.sappi.R;
@@ -15,21 +18,28 @@ import com.lynxsolutions.intern.sappi.R;
  */
 public class RouteDetailFragment extends Fragment {
 
+    private TextView tvRouteDetail;
     Route route;
 
     public RouteDetailFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        getActivity().setTitle("Car Deatail");
+        View view = inflater.inflate(R.layout.fragment_route_detail, container, false);
+        getActivity().setTitle("Car Detail");
         route = (Route) getArguments().get("route");
         Toast.makeText(getContext(), route.getUsername(), Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.fragment_route_detail, container, false);
+        initViews(view);
+        return view;
+    }
+
+    private void initViews(View view) {
+        tvRouteDetail = view.findViewById(R.id.route_detail_tv_description);
+        tvRouteDetail.setMovementMethod(new ScrollingMovementMethod());
     }
 
 }
