41bbe16b60806b951e5bc6ad01b36385f8dda886
Takacs Kalman <kalmy07@yahoo.com>
2017-08-04 16:57:49 +0300

    NewsFeed updated

--- app/src/main/java/com/lynxsolutions/intern/sappi/news/NewsFeedFragment.java
+++ app/src/main/java/com/lynxsolutions/intern/sappi/news/NewsFeedFragment.java
@@ -1,10 +1,10 @@
 package com.lynxsolutions.intern.sappi.news;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,53 +19,60 @@ import com.lynxsolutions.intern.sappi.cars.Route;
 import com.lynxsolutions.intern.sappi.events.Event;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewsFeedFragment extends Fragment {
 
-    RecyclerView recyclerView;
-    ComplexRecyclerViewAdapter mAdapter;
-    DatabaseReference databaseReferenceForEvents;
-    DatabaseReference getDatabaseReferenceForCars;
-    Event event;
-    Route route;
-    ArrayList<Object> postContainer = new ArrayList<>();
-    ArrayList<Event> eventContainer = new ArrayList<>();
-    ArrayList<Route> routeContainer = new ArrayList<>();
-    ArrayList<Object> eventAndMapContainer = new ArrayList<>();
-
+    private RecyclerView recyclerView;
+    private DatabaseReference databaseReferenceForEvents;
+    private DatabaseReference getDatabaseReferenceForCars;
+    private Event event;
+    private Route route;
+    private ArrayList<Object> postContainer;
+    private ArrayList<Event> eventContainer;
+    private ArrayList<Route> routeContainer;
+    private View view;
 
     public NewsFeedFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_news_feed, container, false);
+        } catch (InflateException e) {
+
+        }
+
+        initViews();
+        getDataReady();
+
+        return view;
+    }
+
+    private void initViews() {
         getActivity().setTitle("News Feed");
-
-        View v = inflater.inflate(R.layout.fragment_news_feed, container, false);
-
+        postContainer = new ArrayList<>();
+        eventContainer = new ArrayList<>();
+        routeContainer = new ArrayList<>();
         event = new Event();
         route = new Route();
         databaseReferenceForEvents = FirebaseDatabase.getInstance().getReference("events");
         getDatabaseReferenceForCars = FirebaseDatabase.getInstance().getReference("cars");
-        recyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
+        recyclerView = view.findViewById(R.id.recycler_view);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(mLayoutManager);
-
-        getDataReady();
-        return v;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 
     private void getDataReady(){
@@ -76,71 +83,51 @@ public class NewsFeedFragment extends Fragment {
 
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     event = snapshot.getValue(Event.class);
-                    eventAndMapContainer.add(event);
                     eventContainer.add(event);
                 }
+
                 getDatabaseReferenceForCars.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        /*
-                        for(Object obj :eventAndMapContainer){
-                            int i = eventAndMapContainer.indexOf(obj);
-                            Event event = (Event)eventAndMapContainer.get(i);
-                            if(event.ge)
-                        }*/
 
-
-
-                        int j = 1;
                         for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                             route = snapshot.getValue(Route.class);
-//                            eventAndMapContainer.add(j,route);
                             routeContainer.add(route);
-                            j += 2;
                         }
-
-                        j = 0;
-                        int i = 0;
-                        while (i < routeContainer.size() && j < eventContainer.size()){
-                            if(Long.parseLong(routeContainer.get(i).getTimestamp()) < Long.parseLong(eventContainer.get(j).getTimestamp())) {
-                                postContainer.add(routeContainer.get(i));
-                                i++;
-                            }
-                            else {
-                                postContainer.add(eventContainer.get(j));
-                                j++;
-                            }
-                        }
-                        while (i < routeContainer.size()){
-                            postContainer.add(routeContainer.get(i));
-                            i++;
-                        }
-
-                        while(j < eventContainer.size()){
-                            postContainer.add(eventContainer.get(j));
-                            j++;
-                        }
-
-
-                        recyclerView.setAdapter(new ComplexRecyclerViewAdapter(postContainer,getContext()));
-
+                        setUpAdapter();
                     }
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-
                     }
                 });
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
-
-
     }
 
+    private void setUpAdapter() {
+        int j = 0;
+        int i = 0;
+        while (i < routeContainer.size() && j < eventContainer.size()){
+            if(Long.parseLong(routeContainer.get(i).getTimestamp()) < Long.parseLong(eventContainer.get(j).getTimestamp())) {
+                postContainer.add(routeContainer.get(i++));
+            }
+            else {
+                postContainer.add(eventContainer.get(j++));
+            }
+        }
+        while (i < routeContainer.size()){
+            postContainer.add(routeContainer.get(i++));
+        }
 
+        while(j < eventContainer.size()){
+            postContainer.add(eventContainer.get(j++));
+        }
+        Collections.reverse(postContainer);
+        recyclerView.setAdapter(new ComplexRecyclerViewAdapter(postContainer,getContext()));
+    }
 
 }
